99625c09e326f0f70b36e4e48f53cc00b17fb4a4
Arantxa Edwards <37158609+arantxa-e@users.noreply.github.com>
2023-06-02 17:08:03 -0400

    Render map using react-map-gl Add markers for StudySpots

--- src/layout/Map.tsx
+++ src/layout/Map.tsx
@@ -1,30 +1,48 @@
-import mapboxgl from "mapbox-gl";
-import { useEffect, useRef, useState } from "react";
+import MapboxMap, { Source, Layer } from "react-map-gl";
+import { GeoJSONSourceOptions } from "mapbox-gl";
+import { useGetStudySpotsQuery } from "../services/studySpot";
+import { useState, useEffect } from "react";
+import { LayerProps } from "react-map-gl";
 
 export const Map = () => {
-  mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN || "";
+  const { data, error, isLoading } = useGetStudySpotsQuery();
+  const [geojson, setGeojson] = useState<GeoJSONSourceOptions["data"]>();
 
-  const mapContainer = useRef<HTMLDivElement | null>(null);
-  const map = useRef<mapboxgl.Map>();
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
+  const layerStyle: LayerProps = {
+    id: "point",
+    type: "circle",
+    paint: {
+      "circle-radius": 10,
+      "circle-color": "#007cbf",
+    },
+  };
 
   useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current as HTMLElement,
-      style: "mapbox://styles/mapbox/streets-v12",
-      center: [lng, lat],
-      zoom: zoom,
-    });
-  });
+    if (data) {
+      setGeojson({
+        type: "FeatureCollection" as const,
+        features: data?.map((studySpot) => studySpot.location),
+      });
+    }
+  }, [data]);
 
   return (
-    <div
-      ref={mapContainer}
-      className="map-container"
-      style={{ height: "100%" }}
-    />
+    <MapboxMap
+      reuseMaps
+      mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
+      initialViewState={{
+        longitude: -100,
+        latitude: 40,
+        zoom: 3.5,
+      }}
+      style={{ width: "100%", height: "100%" }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    >
+      {geojson && (
+        <Source id="my-data" type="geojson" data={geojson}>
+          <Layer {...layerStyle} />
+        </Source>
+      )}
+    </MapboxMap>
   );
 };
