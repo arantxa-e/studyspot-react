21617cb127031373b31678d275e5e837cad18eb0
Arantxa Edwards <37158609+arantxa-e@users.noreply.github.com>
2023-06-03 17:38:18 -0400

    Add static height

--- src/layout/Map.tsx
+++ src/layout/Map.tsx
@@ -13,48 +13,46 @@ export const Map: React.FC<{
   const { data } = useGetStudySpotsQuery();
 
   return (
-    <>
-      <MapboxMap
-        reuseMaps
-        mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
-        {...viewState}
-        onMove={(evt) => setViewState(evt.viewState)}
-        style={{ width: "100%", height: "100%" }}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-      >
-        {data?.map((studySpot) => {
-          return (
-            <Marker
-              key={studySpot.name}
-              longitude={studySpot.location.geometry.coordinates[0]}
-              latitude={studySpot.location.geometry.coordinates[1]}
-              style={{ cursor: "pointer" }}
-            >
-              <img
-                src={MarkerIcon}
-                alt={studySpot.name}
-                style={{ width: "50px", height: "50px" }}
-              />
-            </Marker>
-          );
-        })}
-
-        {selectedLocation && (
+    <MapboxMap
+      reuseMaps
+      mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
+      {...viewState}
+      onMove={(evt) => setViewState(evt.viewState)}
+      style={{ width: "100%", height: "700px" }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    >
+      {data?.map((studySpot) => {
+        return (
           <Marker
-            longitude={selectedLocation.geometry.coordinates[0]}
-            latitude={selectedLocation.geometry.coordinates[1]}
+            key={studySpot.name}
+            longitude={studySpot.location.geometry.coordinates[0]}
+            latitude={studySpot.location.geometry.coordinates[1]}
             style={{ cursor: "pointer" }}
           >
             <img
               src={MarkerIcon}
-              alt="Selected location"
+              alt={studySpot.name}
               style={{ width: "50px", height: "50px" }}
             />
           </Marker>
-        )}
+        );
+      })}
 
-        <NavigationControl showZoom position="top-right" />
-      </MapboxMap>
-    </>
+      {selectedLocation && (
+        <Marker
+          longitude={selectedLocation.geometry.coordinates[0]}
+          latitude={selectedLocation.geometry.coordinates[1]}
+          style={{ cursor: "pointer" }}
+        >
+          <img
+            src={MarkerIcon}
+            alt="Selected location"
+            style={{ width: "50px", height: "50px" }}
+          />
+        </Marker>
+      )}
+
+      <NavigationControl showZoom position="top-right" />
+    </MapboxMap>
   );
 };
