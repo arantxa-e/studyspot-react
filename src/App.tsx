9f65491a5a1f79bfa5627a2a6d080c304e40d0a4
Arantxa Edwards <37158609+arantxa-e@users.noreply.github.com>
2023-06-04 10:51:36 -0400

    Wrap app in ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { RouterProvider } from "react-router";
 import { router } from "./router";
+import { ThemeProvider, createTheme } from "@mui/material";
+
+const theme = createTheme({});
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
