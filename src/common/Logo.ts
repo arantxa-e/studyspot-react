e31e138d09777a49f4efc392503179052c5a2480
Arantxa Edwards <37158609+arantxa-e@users.noreply.github.com>
2023-06-03 22:12:37 -0400

    Create styled Logo component

---
+++ src/common/Logo.ts
@@ -0,0 +1,8 @@
+import { styled } from "@mui/material";
+
+export const Logo = styled("img")({
+  width: 100,
+  height: 100,
+  borderRadius: 5,
+  boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.2)",
+});
