9174fa6a3018cf9e248d38bb99fc06f9c9cdd274
Arantxa Edwards <37158609+arantxa-e@users.noreply.github.com>
2023-06-04 11:01:19 -0400

    Add sign up and login links

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,7 +1,9 @@
 import { AppBar, Toolbar, Typography, Button } from "@mui/material";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <AppBar position="static">
@@ -10,7 +12,12 @@ export const Navbar = () => {
             <NavLink to="/">StudySpot</NavLink>
           </Typography>
 
-          <Button color="inherit">Sign Up</Button>
+          <Button color="inherit" onClick={() => navigate("/sign-up")}>
+            Sign Up
+          </Button>
+          <Button color="inherit" onClick={() => navigate("/login")}>
+            Log In
+          </Button>
         </Toolbar>
       </AppBar>
     </>
