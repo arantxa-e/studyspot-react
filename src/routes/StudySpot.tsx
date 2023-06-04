c6f883ab356911982f2b2879d55771d58a2f7105
Arantxa Edwards <37158609+arantxa-e@users.noreply.github.com>
2023-06-03 22:13:54 -0400

    Render StudySpot inside StudySpotCard

--- src/routes/StudySpot.tsx
+++ src/routes/StudySpot.tsx
@@ -1,39 +1,13 @@
 import { useLoaderData } from "react-router";
-import {
-  Card,
-  CardActions,
-  CardContent,
-  Typography,
-  Button,
-  Chip,
-  Divider,
-} from "@mui/material";
 import { StudySpot as StudySpotInterface } from "../types";
+import { StudySpotCard } from "../layout";
 
 export const StudySpot = () => {
   const data = useLoaderData() as StudySpotInterface;
 
   return (
-    <Card>
-      <CardContent>
-        <Typography variant="h5" component="h1">
-          {data.name}
-        </Typography>
-        {data.hasFreeWifi && <Chip label="Free Wifi" />}
-
-        <Typography variant="body2" component="div">
-          {data.phoneNumber} • {data.location.properties.address}
-        </Typography>
-
-        <Divider />
-        <Typography variant="body1">{data.description}</Typography>
-      </CardContent>
-
-      <CardActions>
-        <Button size="small" color="primary">
-          Save
-        </Button>
-      </CardActions>
-    </Card>
+    <>
+      <StudySpotCard data={data} />
+    </>
   );
 };
