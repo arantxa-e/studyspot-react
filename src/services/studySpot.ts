bf73374ef39bd211aac3e6d30d85843cafccf12d
Arantxa Edwards <37158609+arantxa-e@users.noreply.github.com>
2023-06-04 11:35:52 -0400

    Add create user endpoint

--- src/services/studySpot.ts
+++ src/services/studySpot.ts
@@ -1,11 +1,13 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { StudySpot } from "../types/studyspot";
+import { User } from "../types/user";
 
 const baseUrl = import.meta.env.VITE_STUDYSPOT_API_BASE_URL;
 
 export const studySpotApi = createApi({
   reducerPath: "studySpotApi",
   baseQuery: fetchBaseQuery({ baseUrl }),
+
   endpoints: (builder) => ({
     getStudySpots: builder.query<Array<StudySpot>, void>({
       query: () => `/studyspots`,
@@ -13,7 +15,14 @@ export const studySpotApi = createApi({
     getStudySpotById: builder.query<StudySpot, string>({
       query: (id) => `/studyspots/${id}`,
     }),
+    createUser: builder.mutation<User, Partial<User>>({
+      query: (payload) => ({ url: "/user", method: "post", body: payload }),
+    }),
   }),
 });
 
-export const { useGetStudySpotsQuery, useGetStudySpotByIdQuery } = studySpotApi;
+export const {
+  useGetStudySpotsQuery,
+  useGetStudySpotByIdQuery,
+  useCreateUserMutation,
+} = studySpotApi;
