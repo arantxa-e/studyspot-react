cc4a5bb706343c494fdec81653c00141cc7edd80
Arantxa Edwards <37158609+arantxa-e@users.noreply.github.com>
2023-06-03 22:13:21 -0400

    Create StudySpotCard component

---
+++ src/layout/StudySpotCard.tsx
@@ -0,0 +1,39 @@
+import {
+  Card,
+  CardContent,
+  Typography,
+  Chip,
+  Divider,
+  Grid,
+} from "@mui/material";
+import { StudySpot as StudySpotInterface } from "../types";
+import { Logo } from "../common";
+
+export const StudySpotCard: React.FC<{ data: StudySpotInterface }> = ({
+  data,
+}) => {
+  return (
+    <Card>
+      <CardContent>
+        <Grid container spacing={2} alignItems="center">
+          <Grid item>
+            <Logo src={data.logo} />
+          </Grid>
+
+          <Grid item>
+            <Typography variant="h5" component="h1">
+              {data.name}
+            </Typography>
+            {data.hasFreeWifi && <Chip label="Free Wifi" />}
+            <Typography variant="body2">
+              {data.phoneNumber} • {data.address}
+            </Typography>
+          </Grid>
+        </Grid>
+
+        <Divider />
+        <Typography variant="body1">{data.description}</Typography>
+      </CardContent>
+    </Card>
+  );
+};
--- src/layout/index.ts
+++ src/layout/index.ts
@@ -1,3 +1,4 @@
 export * from "./Map";
 export * from "./Search";
 export * from "./Navbar";
+export * from "./StudySpotCard";
