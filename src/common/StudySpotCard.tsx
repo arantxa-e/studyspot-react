62921acad14181b4e01f0dfcafa1e460e853f456
Arantxa Edwards <37158609+arantxa-e@users.noreply.github.com>
2023-06-04 10:26:56 -0400

    Make card clickable if not extended

--- src/common/StudySpotCard.tsx
+++ src/common/StudySpotCard.tsx
@@ -5,36 +5,56 @@ import {
   Chip,
   Divider,
   Grid,
+  CardActionArea,
 } from "@mui/material";
 import { StudySpot as StudySpotInterface } from "../types";
 import { Logo, BusinessHours } from ".";
+import { useNavigate } from "react-router";
+
+const renderDetails = (data: StudySpotInterface) => {
+  return (
+    <CardContent>
+      <Grid container spacing={2} alignItems="center">
+        <Grid item>
+          <Logo src={data.logo} />
+        </Grid>
+
+        <Grid item>
+          <Typography variant="h5" component="h1">
+            {data.name}
+          </Typography>
+          {data.hasFreeWifi && <Chip label="Free Wifi" />}
+          <Typography variant="body2">
+            {data.phoneNumber} • {data.address}
+          </Typography>
+        </Grid>
+      </Grid>
+
+      <Divider />
+      <Typography variant="body1">{data.description}</Typography>
+    </CardContent>
+  );
+};
 
 export const StudySpotCard: React.FC<{
   data: StudySpotInterface;
   extended?: boolean;
 }> = ({ data, extended = false }) => {
+  const navigate = useNavigate();
+
   return (
     <Card>
+      {extended ? (
+        renderDetails(data)
+      ) : (
+        <CardActionArea
+          onClick={() => navigate(`/studyspot/${data._id}`)}
+          disableRipple
+        >
+          {renderDetails(data)}
+        </CardActionArea>
+      )}
       <CardContent>
-        <Grid container spacing={2} alignItems="center">
-          <Grid item>
-            <Logo src={data.logo} />
-          </Grid>
-
-          <Grid item>
-            <Typography variant="h5" component="h1">
-              {data.name}
-            </Typography>
-            {data.hasFreeWifi && <Chip label="Free Wifi" />}
-            <Typography variant="body2">
-              {data.phoneNumber} • {data.address}
-            </Typography>
-          </Grid>
-        </Grid>
-
-        <Divider />
-        <Typography variant="body1">{data.description}</Typography>
-
         {extended && (
           <>
             <Divider />
