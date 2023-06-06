7d50ed57133355f1d0b95721e23845cda226577e
Arantxa Edwards <37158609+arantxa-e@users.noreply.github.com>
2023-06-06 00:40:02 -0400

    Render UI conditionally based on user auth status

--- src/common/Reviews.tsx
+++ src/common/Reviews.tsx
@@ -1,11 +1,14 @@
 import { Typography, Link } from "@mui/material";
+import { NavLink } from "react-router-dom";
 import { Review, ReviewForm } from ".";
 import { useState } from "react";
 import { Review as ReviewInterface } from "../types";
+import { useAppSelector } from "../hooks";
 
 export const Reviews: React.FC<{ reviews?: ReviewInterface[] }> = ({
   reviews,
 }) => {
+  const user = useAppSelector((state) => state.auth.user);
   const [displayRatingForm, setDisplayRatingForm] = useState<boolean>(false);
   const toggleDisplayRatingForm = () =>
     setDisplayRatingForm(!displayRatingForm);
@@ -16,14 +19,23 @@ export const Reviews: React.FC<{ reviews?: ReviewInterface[] }> = ({
         Reviews
       </Typography>
 
-      <Link component="button" onClick={toggleDisplayRatingForm}>
-        {displayRatingForm ? "Cancel" : "Add review"}
-      </Link>
+      {!user && (
+        <>
+          <NavLink to="/sign-up">Sign up</NavLink> or{" "}
+          <NavLink to="/login">login</NavLink> to leave a review.
+        </>
+      )}
+
+      {user && (
+        <Link component="button" onClick={toggleDisplayRatingForm}>
+          {displayRatingForm && user ? "Cancel" : "Add review"}
+        </Link>
+      )}
 
       {displayRatingForm && <ReviewForm />}
 
       {reviews?.map((review) => (
-        <Review review={review} />
+        <Review review={review} key={review._id} />
       ))}
 
       {!reviews?.length && "No reviews yet."}
